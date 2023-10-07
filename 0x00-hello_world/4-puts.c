2e0b52de6573a905ead4f048e7fddc656e88a34e
iyanuloko <iyanuoluwaloko@gmail.com>
2023-10-07 14:42:00 +0000

    Puts retry

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+/**
+ * main - Prints ""Programming is like building a multilingual puzzle"
+*/
 int main(void)
 {
 char Mystr2[100] = "\"Programming is like building a multilingual puzzle";
