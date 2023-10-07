db48293b7d49c35a03c94fdb688d82786a10a3d1
iyanuloko <iyanuoluwaloko@gmail.com>
2023-10-07 14:25:00 +0000

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-int main()
+int main(void)
 {
-    char Mystr2[100] = "\"Programming is like building a multilingual puzzle";
-    puts(Mystr2);
-    return 0;
+char Mystr2[100] = "\"Programming is like building a multilingual puzzle";
+    
+puts(Mystr2);
+return 0;
 }
