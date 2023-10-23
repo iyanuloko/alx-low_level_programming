1d6a1e35b3a37568e45c471bd3b0d5cdd7a5dc53
iyanuloko <iyanuoluwaloko@gmail.com>
2023-10-23 05:51:23 +0000

    Making it Betty compliant

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 /**
  * main - Prints ""Programming is like building a multilingual puzzle"
+ *
+ * Return: Always 0
 */
 int main(void)
 {
@@ -8,4 +10,3 @@ char Mystr2[100] = "\"Programming is like building a multilingual puzzle";
 puts(Mystr2);
 return (0);
 }
-
